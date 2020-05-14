dd760d42d5c78593d3038bc286b55a01dbf42449
Pablo Freire <pablofreire6@gmail.com>
2020-05-14 01:13:38 -0300

    header added to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Sidebar from "../layout/Sidebar";
+import Header from "../layout/Header";
 
 const Projects = () => {
   return (
     <div className="contenedor-app">
       <Sidebar />
       <div className="seccion-principal">
+        <Header />
         <main>
           <div className="contenedor-tareas"></div>
         </main>
