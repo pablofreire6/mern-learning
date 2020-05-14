5af9fcc1f77a54aaf159dfa37c86cf1248bf3786
Pablo Freire <pablofreire6@gmail.com>
2020-05-14 00:55:01 -0300

    set state for new projects

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,6 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NewProject = () => {
+  const [project, setProject] = useState({
+    nombre: "",
+  });
+
+  const { nombre } = project;
+
+  const onChangeProject = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
@@ -12,6 +25,8 @@ const NewProject = () => {
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProject}
         />
         <input
           type="submit"
